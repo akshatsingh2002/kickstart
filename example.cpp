c6f52a8e4f516a475c655d0f57623726fb0e5bf2
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-02-25 19:14:39 +0530

    commit

--- example.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-long c,p;
-int main(){
-    cin>>c>>p;
-    
-    return 0;
-}
---
+++ stl1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enetr the size of the array";
+    cin>>n;
+    int *arr=new int(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}

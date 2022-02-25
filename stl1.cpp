e2ae848fe55afce14ffb4f30a146f8908c3bf7db
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-02-25 22:33:21 +0530

    commit

--- stl1.cpp
+++ stl1.cpp
@@ -8,9 +8,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+ 
     cout<<"Enter the new size "<<endl;
     cin>>n;
     arr=new int(n);//just changing size//

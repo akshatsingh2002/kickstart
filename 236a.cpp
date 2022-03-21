e182a34832585f696d7d8adf539393fcf03b6782
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-03-21 20:37:36 +0530

    commit

--- 236a.cpp
+++ 236a.cpp
@@ -3,21 +3,26 @@ using namespace std;
 int main(){
     string s;
     int count=0,rep=0;
-    cin>>s;
+ cin>>s;
     int l=s.length();
     sort(s.begin(),s.end()-1);
-    cout<<s;
+
     for(int i=0;i<l-1;i++){
-        
+        count=0;
         for(int j=i+1;j<l;j++){
-            count=1;
+            
         if(s.at(i)==s.at(j)){
-        break;
+            count++;
         
         }
         }
-       rep=count+rep;
+       rep=rep+count;
     }
-    cout<<rep;
+ 
+int ans=l-rep;
+if(ans%2==0)
+cout<<"CHAT WITH HER!";
+else 
+cout<<"IGNORE HIM!";
     return 0;
 }

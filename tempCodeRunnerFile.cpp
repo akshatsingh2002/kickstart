8cc1cba75c4ae5692849009b4e0281bdfe09ef56
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-04-04 18:16:44 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,15 +2,51 @@
 using namespace std;
 int main(){
     string s;
+    long long n=0,con1=0,k=0;
+    int f=0;
     cin>>s;
-    transform(s.begin(),s.end(),s.begin(),::tolower);
-    int l=s.length();
-    
-    for(int i=0;i<l;i++){
-      if(s.at(i)!='a'&&s.at(i)!='e'&&s.at(i)!='i'&&s.at(i)!='o'&&s.at(i)!='u'&&s.at(i)!='y')
-      cout<<"."<<s.at(tolower(i));
-     
+    long long l=s.length();
+    for(long long i=0;i<l;i++){
+        if(s.at(i)=='7'){
+            f=1;
+            con1=1;
+            
+            }
+             if(s.at(i)=='4'){
+            f=1;
+        
+            }
+            else{
+           k=1;
+            }
+          
+        
     }
+    for (long long i=0;i<l;i++){
+        if(s.at(i)=='4'||s.at(i)=='7'){
+            n++;
+        }
+    }
+    if(k==1){
+string g=to_string(n);
+long long k=g.length();
+for(long long i=0;i<k;i++){
+        if(g.at(i)=='7'||g.at(i)=='4'){
+            f=1;
+            }
+            else {
+                f=0;
+            break;
+            }
+        
+    }
+    }
+  
+
+if(f==1&&l>=2&&con1==1)
+cout<<"YES";
+else
+cout<<"NO";
     
     return 0;
 }

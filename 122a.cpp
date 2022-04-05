3fed49f38433cdfe0095358cb708b598823930c3
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-04-05 21:03:16 +0530

    commit

--- 122a.cpp
+++ 122a.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    long long n;
+    long long n,f=0;
     cin>>n;
     if(n%4==0)
     cout<<"YES";
@@ -9,7 +9,21 @@ int main(){
     cout<<"YES";
     else if(n%47==0)
     cout<<"YES";
-    else 
+    else{
+    while(n>0){
+        if(n%10==7||n%10==4){
+            n=n/10;
+            f=1;
+        }
+        else{
+            f=0;
+            break;
+        }
+    }
+    if(f==1)
+    cout<<"YES";
+    else
     cout<<"NO";
+    }
     return 0;
 }

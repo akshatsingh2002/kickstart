1c59aeebbf0ed1da6d18c43abf38cf1dcee8c8a5
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-04-08 10:01:30 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,52 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s;
-    long long n=0,con1=0,k=0;
-    int f=0;
-    cin>>s;
-    long long l=s.length();
-    for(long long i=0;i<l;i++){
-        if(s.at(i)=='7'){
-            f=1;
-            con1=1;
-            
-            }
-             if(s.at(i)=='4'){
-            f=1;
-        
-            }
-            else{
-           k=1;
-            }
-          
-        
+    long long n,k;
+    cin>>n>>k;
+    long long arr[n];
+    long long a=1;
+    long long count1;
+    if(n&2!=0)
+    count1=(n/2)+1;
+    else 
+    count1=n/2;
+    for(long long i=0;i<count1;i++){
+        arr[i]=a;
+        a=a+2;
     }
-    for (long long i=0;i<l;i++){
-        if(s.at(i)=='4'||s.at(i)=='7'){
-            n++;
-        }
+    a=2;
+     for(long long i=count1;i<n;i++){
+        arr[i]=a;
+        a=a+2;
     }
-    if(k==1){
-string g=to_string(n);
-long long k=g.length();
-for(long long i=0;i<k;i++){
-        if(g.at(i)=='7'||g.at(i)=='4'){
-            f=1;
-            }
-            else {
-                f=0;
-            break;
-            }
-        
-    }
-    }
-  
-
-if(f==1&&l>=2&&con1==1)
-cout<<"YES";
-else
-cout<<"NO";
-    
+    cout<<arr[k-1];
     return 0;
 }

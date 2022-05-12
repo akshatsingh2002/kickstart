1b9020c6245650e6c39f8553991526d1d0253f16
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-05-12 18:24:01 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s;
-  getline(cin,s);
-  long l=s.length();
-  long count=0;
-  for(long long i=2;i<l;){
-    if(s.at(i)==s.at(i-2))
-    count++;
-    i=i+2;
-  }
-  cout<<count;
-  return 0;
+    long long i;
+    cin>>i;
+    long long arr[i];
+    for(long long l=0;l<i;l++){
+        cin>>arr[l];
+    }
+    sort(arr,arr+i);
+       for(long long l=0;l<i;l++){
+        cout<<arr[l]<<" ";
+    }
+
+
+    return 0;
 }

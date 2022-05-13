c05dc4ed6b1019283e462506df2b505734d44b64
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-05-13 13:57:08 +0530

    commit

--- 469a.cpp
+++ 469a.cpp
@@ -1,32 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-int n,count=1;
-cin>>n;
- vector<int> arr;
-  long long a,b;
-  int data;
-  cin>>a;
-  for(long long i=0;i<a;i++){
-      cin>>data;
-      arr.push_back(data);
-  }
-   cin>>b;
-  for(long long i=0;i<b;i++){
-       cin>>data;
-      arr.push_back(data);
-  }
-sort(arr.begin(),arr.end()-1);
-for(int i=0;i!=arr.size();i++){
-    if(arr.at(i)!=arr.at(i-1)){
-        count++;
-    }
+    int count=1,n;
+    cin>>n;
+    vector<int> arr;
+    int a,b;
+cin>>a;
+for(int i=0;i<a;i++){
+    cin>>b;
+    arr.push_back(b);
 }
-     
-   if(count==n){
-       cout<<"I become the guy.";
-   }
-   else
-   cout<<"Oh, my keyboard!";
+cin>>a;
+for(int i=0;i<a;i++){
+    cin>>b;
+    arr.push_back(b);
+}
+sort(arr.begin(),arr.end()-1);
+int s=arr.size();
+for(int i=1;i<arr.size();i++){
+    if(arr.at(i)!=arr.at(i-1))
+    count++;
+    
+}
+if(count==n)
+cout<<"I become the guy.";
+else
+cout<<"Oh, my keyboard!";
     return 0;
 }

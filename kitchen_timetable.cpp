11f480655f8ea2b69473a763fb4cf7c68ce701b4
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-09-21 18:20:08 +0530

    commit

--- kitchen_timetable.cpp
+++ kitchen_timetable.cpp
@@ -1,35 +1,28 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
-    int n;
-    int j,count=0;
-    cin>>n;
-   
-    for(int i=0;i<n;i++){
-        cin>>j;
-         int *arr1 = new int[j];
-         int *arr2 = new int[j];
-        for(int k=0;k<j;k++){
-            cin>>arr1[k];
+    int t,count=0;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n=0;
+        cin>>n;
+        n=n+1;
+        int arr1[n],arr2[n];
+        arr1[0]=0;
+        arr2[0]=0;
+        for(int j=1;j<=n;j++){
+            cin>>arr1[j];
         }
-        for(int k=0;k<j;k++){
-            cin>>arr2[k];
+        for(int j=1;j<=n;j++){
+            cin>>arr2[j];
         }
-         for(int f=0;f<j;f++){
-        if(f==0){
-            if(arr1[f]>=arr2[f])
+        for(int j=1;j<=n;j++){
+            if((arr1[j]-arr1[j-1])>=arr2[j])
             count++;
         }
-        else if((arr1[f]-arr1[f-1])>=arr2[f])
-        count++;
+        
+        cout<<count;
+        count=0;
     }
-    cout<<count;
-    delete arr1,arr2;
-    count=0;
-
-
-    }
-   
-
-return 0;
+    return 0;
 }

8fbd97ce2423d5ab62a7336ec1f3932898d0b04f
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-09-22 17:29:40 +0530

    commit

--- frgame.cpp
+++ frgame.cpp
@@ -6,13 +6,22 @@ int main(){
     for(int i=0;i<n;i++){
         int N,S,R,SA;
         cin>>N>>S>>R>>SA;
+        if(N>=S){
+            S=S+R;
+        }
+        else
         N=N+R;
-        S=S+SA;
+        if(N>=S){
+            S=S+SA;
+        }
+        else
+        N=N+SA;
+
         if(N>=S){
             cout<<"N"<<endl;
-    }
-    else
-    cout<<"S"<<endl;
+        }
+        else
+        cout<<"S"<<endl;
     }
     return 0;
 }

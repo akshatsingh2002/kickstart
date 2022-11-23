feec137f6e0e0b8e77810b96cdc950881d1594e1
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-11-23 22:19:34 +0530

    commit

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -1,3 +1,10 @@
+//Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+//An integer n is a power of two, if there exists an integer x such that n == 2x.
+#include<iostream>
+#include<math.h>
+
+using namespace std;
     bool isPowerOfTwo(int n) {
         int j = 2;
         if(n==1 || n == 2){
@@ -17,4 +24,10 @@
         return false;
 
         
+    }
+    int main(){
+        int n;
+        cin>>n;
+        cout<<isPowerOfTwo(n);
+        return 0;
     }

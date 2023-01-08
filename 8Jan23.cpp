1fe1c94c53922cd3ecba4f2bb83dcb48883a370c
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-01-08 21:34:48 +0530

    commit

--- 8Jan23.cpp
+++ 8Jan23.cpp
@@ -1,44 +1,74 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-void ugly()
-{
+// void ugly()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     int sum = 0 , flag = 0,temp;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+       
+//     }
+//     for(int i=0;i<n;i++){
+//          if(i>0 && arr[i]==arr[i-1]){
+//             flag = 0;
+//         }
+//         else if(i>0){
+//             temp = arr[0];
+//             flag = 1;
+//             break;
+//         }
+//     }
+//     if(flag == 0)
+//     cout<<"NO"<<endl;
+//     else{
+//         cout<<"YES"<<endl;
+//         cout<<temp<<" ";
+//         for(int i=n-1;i>=1;i--){
+//             cout<<arr[i]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     while (n != 0)
+//     {
+//         ugly();
+//         n = n - 1;
+//     }
+//     return 0;
+// }
+
+void matrix(){
     int n;
-    cin >> n;
-    int arr[n];
-    int sum = 0 , flag = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-        
-        
-        if (arr[i] == sum && i>0 && arr[i]!=arr[i-1]){
-            swap(arr[i],arr[i-1]);
-            flag = 1 ;
+    cin>>n;
+    int arr[n][n];
+    int t = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0){
+                cout<<n*n<<" ";
+            }
+            else
+            cout<<t<<" ";
+            t = t + 1;
+            }
+            cout<<endl;
         }
+    }
 
-        sum = sum + arr[i];
-    }
-    if(flag == 1 || (n==3&&arr[0]==1&&arr[1]==4)&&arr[2]==4){
-        cout<<"YES"<<endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout<<endl;
-    }
-    else
-    cout<<"NO"<<endl;
-
-}
-int main()
-{
+int main(){
     int n;
-    cin >> n;
-    while (n != 0)
-    {
-        ugly();
-        n = n - 1;
+    cin>>n;
+    while(n!=0){
+        matrix();
+        n =n -1;
     }
-    return 0;
 }

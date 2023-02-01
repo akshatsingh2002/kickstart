eba7c823e51bdd20967485bcdb0d843335459b1c
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-02-01 19:25:16 +0530

    commit

--- 337a.cpp
+++ 337a.cpp
@@ -1,57 +1,30 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include<iostream>
+#include<algorithm>
 using namespace std;
-int main()
-{
-
-    int n, m;
-    cin >> n >> m;
-    long long int arr[m];        // input array
-    long long int values[m - n]; // array to sotre the difference between the highes and lowest value
-    int max_val, min_val;
-
-    for (long long i = 0; i < m; i++)
-    {
-        cin >> arr[i];
+void puzzles(){
+    int n,m;
+    cin>>n>>m;
+    int arr[m];
+    int sol[m-n+1];
+    for(int i=0;i<m;i++){
+        cin>>arr[i];
     }
-    for (long long i = 0; i < m - n + 1 ; i++)
-    {
-        max_val = 4;
-        min_val = 1000;
-        
-        for (long long j = i; j < i + n  ; j++)
-        {
-            // cout<<i<<"i"<<j<<'J'<<endl;
-            
-
-            if (arr[j] >= max_val)
-            {
-                max_val = arr[j];
-            }
-            if (arr[j] <= min_val)
-            {
-                min_val = arr[j];
-            }
-
-            // cout<<max_val<<" "<<min_val<<endl;
+    
+    for(int i=0;i<m-n+1;i++){
+        int max=0,min=10000;
+        for(int j=i;j<n+i;j++){
+            if(arr[j]>max)
+            max = arr[j];
+            if(arr[j]<min)
+            min = arr[j];
         }
-        // cout<<max_val-min_val<<"ans";
-        values[i] = max_val - min_val;
+        // cout<<"max"<<max<<"min"<<min<<endl;
+        sol[i] = max - min;
     }
-    int min_val2 = 1000;
-    for (long long i = 0; i < m - n + 1; i++)
-    {
-        cout<<values[i]<<endl;
-
-        if (values[i] <= min_val2)
-        {
-            min_val2 = values[i];
-            values[i];
-        }
-    }
-
-    cout << min_val2 << endl;
-
+    sort(sol,sol+m-n+1);
+    cout<<sol[0]<<endl;
+}
+int main(){
+     puzzles();
     return 0;
 }

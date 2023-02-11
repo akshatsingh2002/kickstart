bec7e4bbb78c4d6a4e27569e9a2a79bec606d004
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-02-11 16:04:08 +0530

    Commmit

--- tPrimes.cpp
+++ tPrimes.cpp
@@ -1,31 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void prime(){
+void prime()
+{
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         int val;
-        int ans=0;
-        cin>>val;
-        for(int i=1;i<val/2+1;i++){
-            if(val%i==0){
-                ans++;
-            }
-            if(ans>=4){
-                break;
-            }
- 
+        int ans = 2;
+        cin >> val;
+        if (val <= 3)
+        {
+            cout << "NO" << endl;
         }
-        ans = ans + 1;
-        if(ans == 3){
-            cout<<"YES"<<endl;
+        else if (val == 4)
+        {
+            cout << "YES" << endl;
+        }
+        else if (val % 2 == 0)
+        {
+            cout << "NO" << endl;
         }
         else
-        cout<<"NO"<<endl;
+        {
+            for (int j = 3; j < val / 2 + 1;)
+            {
+                if (val % j == 0)
+                {
+                    ans++;
+                }
+                if (ans >= 4)
+                {
+                    break;
+                }
+                j = j + 2;
+            }  
+            if(ans == 3)
+
+            cout << "YES" << endl;
+            else
+            cout<<"NO"<<endl;
+        }
     }
-    
 }
-int main(){
+int main()
+{
     prime();
     return 0;
 }

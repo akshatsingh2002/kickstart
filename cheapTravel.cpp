e63dc794b3021b84498bf7a64c978c58b25d7fba
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-02-21 12:12:14 +0530

    commit

--- cheapTravel.cpp
+++ cheapTravel.cpp
@@ -5,27 +5,28 @@ void cheap()
     int n, m, a, b;
     cin >> n >> m >> a >> b;
     int min = 1000000;
-    int cost = 0;   
+
+    int cost = 0;
     while (n > 0)
     {
-        if (n - m <= 0 && cost + a > cost + b )
+        if (b / m < a && n - m <= 0 && cost + (a*n) > cost + b)
         {
             cost = cost + b;
+            n = n - m;
             break;
         }
-        if (b / m < a && n - m >= 0)
+        if (b / m < a && n-m>=0)
         {
             cost = cost + b;
             n = n - m;
         }
-
         else
         {
             cost = cost + a;
             n = n - 1;
         }
-        // cout << cost << endl;
     }
+
     cout << cost << endl;
 }
 

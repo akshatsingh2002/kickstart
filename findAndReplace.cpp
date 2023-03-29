97d76fb2aab2b13f41b7ff0fe1f70a10807df23c
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-03-29 09:38:54 +0530

    commit

--- findAndReplace.cpp
+++ findAndReplace.cpp
@@ -8,7 +8,7 @@ void replace()
     string s;
     cin >> s;
     int even = 1;
-    int con = 0 ;
+    int con = 0;
     for (int i = 0; i < n; i++)
     {
         char val = s[i];
@@ -25,9 +25,25 @@ void replace()
             }
         }
         even++;
-  
     }
-    cout<<s<<endl;
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            con = 0;
+            break;
+        }
+        else
+        {
+            con = 1;
+        }
+    }
+    if (con == 1  || n == 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+        cout << "NO" << endl;
 }
 int main()
 {

2f2cb8764ac60a498f0031bb354af756bfbd8cdf
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-03-30 21:26:12 +0530

    commit

--- Garland.cpp
+++ Garland.cpp
@@ -7,17 +7,28 @@ void garland()
     string s;
     cin >> s;
     sort(s.begin(), s.end());
-    if(s[0]==s[1] && s[1]==s[2] && s[2]==s[3]){
-        cout<<-1<<endl;
+    if (s[0] == s[1] && s[0] == s[1] && s[0] == s[3])
+    {
+        cout << -1 << endl;
     }
-    else if(s[1]!=s[0] && s[0] !=s[2]!=s[3]){
-        cout<<4<<endl;
-    } 
-    else if (s[0]==s[1] || s[1]==s[2] || s[2]==s[3]){
-        cout<<4<<endl;
-    }
-    else {
-        cout<<6<<endl;
+    else
+    {
+        int dis = 1;
+        for (int i = 1; i < 4; i++)
+        {
+            if (s[i] != s[i - 1])
+            {
+                dis++;
+            }
+        }
+        if (dis >= 4)
+        {
+            cout << 4 << endl;
+        }
+        else
+        {
+            cout << 6 << endl;
+        }
     }
 }
 

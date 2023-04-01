6429500a3209dc5d9ce525dc3b0ff036d53a0a6b
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-01 16:07:53 +0530

    commit

--- firstAndLastOccuranceUsingBst.cpp
+++ firstAndLastOccuranceUsingBst.cpp
@@ -3,38 +3,23 @@
 using namespace std;
 void bst(int arr[], int size, int val)
 {
-    int start = 0;
-    int end = size - 1;
-    int mid = start + (end - start) / 2;
-    int con = 0;
-    while (start <= end)
-    {
-        if (arr[start] == val && arr[end] == val)
-        {
-            con = 1;
+    int intial=-1,final1=-1;
+    for(int i=0;i<size;i++){
+        if(intial == - 1){
+            if(arr[i] == val){
+                intial = i;
+            }
+        }
+        if(intial != -1 && arr[i] != val){
+            final1 = i - 1;
             break;
         }
-        else if (val < arr[mid])
-        {
-            end = mid - 1;
-        }
-        else
-        {
-            start = mid + 1;
-        }
-        mid = start + (end - start) / 2;
     }
-    if (con == 1)
-    {
-        cout << "start" << start << " "
-             << "End" << end;
-    }
-    else
-        cout << "element not ound " << endl;
+    cout<<intial<< " "<<final1<<endl; 
 }
 int main()
 {
-    int arr[10] = {1, 2, 3, 3, 3, 4, 5, 6, 7, 8};
-    bst(arr, 10, 3);
+    int arr[10] = {1, 2, 3, 3, 3, 5, 5, 6, 7, 8};
+    bst(arr, 10, 5);
     return 0;
 }

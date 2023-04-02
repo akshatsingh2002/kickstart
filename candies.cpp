63b70d4b6a9093cfcbeac1c75e2d1bb35960d9fb
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-02 20:19:07 +0530

    commit

--- candies.cpp
+++ candies.cpp
@@ -1,21 +1,47 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void candies(){
+void candies()
+{
     int n;
-    cin>>n;
-    int val = 1;
-    vector<int> arr;
-    int pc = 1;
-    while(pc<=40){
-        if(2*val + 1 == n){
-            arr.push_back(1);
-            val = 2*val + 1;
+    cin >> n;
+    if (n % 2 != 0)
+    {
+        vector<int> binary;
+        int i = 0;
+        while (n > 0)
+        {
+            binary.push_back(n % 2);
+            n = n / 2;
+            i++;
         }
-        else if(2*val - 1 == n){
-            arr.push_back(2);
-            val 
+        cout<<binary.size()-1<<endl;
+        for (int i = binary.size()-1; i > 0 ; i--)
+        {
+            if (binary[i] == 1)
+            {
+                cout << 2 << " ";
+            }
+            else
+            {
+                cout << 1 << " ";
+            }
         }
-
+        cout << endl;
     }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+int main()
+{
+    int k = 0;
+    cin >> k;
+    while (k!=0)
+    {
+        candies();
+        k = k - 1;
+    }
+    return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-void eren()
+void candies()
 {
-    int n, a, b;
-    cin >> n >> a >> b;
-    int sum = 0;
-    for (int i = 1; i <= n; i++)
+    int n;
+    cin >> n;
+    if (n % 2 != 0)
     {
-        if (i % 2 == 0)
+        vector<int> binary;
+        int i = 0;
+        while (n > 0)
         {
-            sum = sum + b;
+            binary.push_back(n % 2);
+            n = n / 2;
+            i++;
         }
-        else
-            sum = sum + a;
+        cout<<binary.size()-1<<endl;
+        for (int i = 0; i < binary.size() - 1; i++)
+        {
+            if (binary[i] == 1)
+            {
+                cout << 2 << " ";
+            }
+            else
+            {
+                cout << 1 << " ";
+            }
+        }
+        cout << endl;
     }
-    cout << sum << endl;
+    else
+    {
+        cout << -1 << endl;
+    }
 }
 int main()
 {
-    int k;
+    int k = 0;
     cin >> k;
-    while (k != 0)
+    while (k!=0)
     {
-        eren();
+        candies();
         k = k - 1;
     }
     return 0;

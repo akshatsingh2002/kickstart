9b60f066a4411b27095503e02bc632ed62240701
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-05 14:00:51 +0530

    commit

--- searchInRotatedSortedArray.cpp
+++ searchInRotatedSortedArray.cpp
@@ -1,60 +1,60 @@
 #include <iostream>
 using namespace std;
-// int findPivot(int arr[], int size)
-// {
-//     int start = 0;
-//     int end = size - 1;
-//     int mid = start + (end - start) / 2;
-//     while (start < end)
-//     {
-//         if (arr[mid] < arr[0])
-//         {
-//             end = mid;
-//         }
-//         else
-//         {
-//             start = mid + 1;
-//         }
-//         mid = start + (end - start) / 2;
-//     }
-//     return end;
-// }
-// int rotated(int arr[], int size, int k)
-// {
-//     int start = 0 ;
-//     int end = size - 1;
-//     int pivot = findPivot(arr, size);
-//     if(pivot == size - 1 && arr[pivot]>arr[0]){
-//     start = 0;
-//     end = size - 1;
-//     }
-//     else{
-//         if(k>arr[size - 1]){
-//             start = 0;
-//             end = pivot - 1;
-//         }
-//         else {
-//             start = pivot;
-//             end = size - 1;
-//         }
-//     }
-//     int mid = start + (end - start) / 2;
-//     while(start<=end){
-//         if(arr[mid]==k){
-//             return mid;
-//         }
-//         else if(k>arr[mid]){
-//             start = mid + 1;
-//         }
-//         else {
-//         end = mid - 1;
-//         }
+int findPivot(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end - start) / 2;
+    while (start < end)
+    {
+        if (arr[mid] < arr[0])
+        {
+            end = mid;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return end;
+}
+int rotated(int arr[], int size, int k)
+{
+    int start = 0 ;
+    int end = size - 1;
+    int pivot = findPivot(arr, size);
+    if(pivot == size - 1 && arr[pivot]>arr[0]){
+    start = 0;
+    end = size - 1;
+    }
+    else{
+        if(k>arr[size - 1]){
+            start = 0;
+            end = pivot - 1;
+        }
+        else {
+            start = pivot;
+            end = size - 1;
+        }
+    }
+    int mid = start + (end - start) / 2;
+    while(start<=end){
+        if(arr[mid]==k){
+            return mid;
+        }
+        else if(k>arr[mid]){
+            start = mid + 1;
+        }
+        else {
+        end = mid - 1;
+        }
 
-//         mid = start +(end - start ) / 2;
-//     }
+        mid = start +(end - start ) / 2;
+    }
 
-//     return - 1;
-// }
+    return - 1;
+}
 
 int main()
 {

0a361b29e2380a6523e971351471d50df3cd38c0
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-06 21:16:17 +0530

    commit

--- coins.cpp
+++ coins.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 void coins(){
-    int fval,k;
+    long long  fval,k;
     cin>>fval>>k;
     if(fval % 2 == 0){
         cout<<"YES"<<endl;
     }
-    else if(k%2==0){
+    else if(k%2==0 || fval == 0){
         cout<<"NO"<<endl;
     }
     else{
---
+++ longLegs.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void legs(){
+    long long  a,b;
+    long long ans=0;
+    cin>>a>>b;
+    if(a==b){
+        cout<<a+b<<endl;
+    }
+    else if(a>b){   
+        ans = ans + b;
+        
+
+    }
+    }
+}

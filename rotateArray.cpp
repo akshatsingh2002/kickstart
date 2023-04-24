d8e68d1a414c3ab4c936b2d27c1ce1660afa9250
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-24 16:41:55 +0530

    commit

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -1,25 +1,31 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
- void rotate(vector<int>& nums, int k) {
-        vector<int> arr;
-        if(k>nums.size()){
-            k = k%nums.size();
-        }
-        for(int i=nums.size()-k;i<nums.size();i++){
-            arr.push_back(nums[i]);
-        }
-        for(int i=0;i<nums.size()-k;i++){
-            arr.push_back(nums[i]);
-        }
-        for(int i=0;i<arr.size();i++){
-            nums[i] = arr[i];
-            cout<<(nums[i])<<" ";
-        }
+void rotate(vector<int> &nums, int k)
+{
+    vector<int> arr;
+    if (k > nums.size())
+    {
+        k = k % nums.size();
     }
+    for (int i = nums.size() - k; i < nums.size(); i++)
+    {
+        arr.push_back(nums[i]);
+    }
+    for (int i = 0; i < nums.size() - k; i++)
+    {
+        arr.push_back(nums[i]);
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        nums[i] = arr[i];
+        cout << (nums[i]) << " ";
+    }
+}
 
-int main(){
-    vector<int> arr = {1,2,3,4,5,6,7,8,9,11};
-    rotate(arr,3);
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
+    rotate(arr, 3);
     return 0;
 }

d372d3c535b90f3468ae4bee487356e68278fb8a
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-04-27 07:51:05 +0530

    commit

--- lowerCase.cpp
+++ lowerCase.cpp
@@ -1,13 +1,45 @@
 #include<iostream>
 #include<algorithm>
 #include<string>
-
 using namespace std;
-int main(){
-    string su;
-    cin>>su;
-    transform(su.begin(), su.end(), su.begin(), ::tolower);
-    cout<<su;
-    return 0;
+bool checkPalindrome(string s){
+    int con = 0;
+    for(int i=0;i<s.length();i++){
+        if(int(s[i])>=48 && int(s[i])<=57){
+            continue;
+        }
+        else if(int(s[i])>=97 && int(s[i])<=122){
+            continue;
+        }
+        else{
+            s.erase(i,1);
+        }
+    }
+    cout<<s<<endl;
+    int i=0;
+    int l = s.length()-1;
+    while(i<l){
+        cout<<i<<" "<<l<<endl;
+        cout<<s[i]<<" "<<s[l]<<endl;
+        if(s[i]==s[l]){
+            con = 1;
+        }
+        else{
+            con = 0;
+            break;
+        }
+        i++;
+        l--;
+    }
+    return con;
 
+}
+
+
+int main(){
+    string s;
+    getline(cin,s);
+    transform(s.begin(),s.end(),s.begin(),::tolower);
+    cout<<checkPalindrome(s);
+    return 0;
 }

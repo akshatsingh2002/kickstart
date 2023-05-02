a926392fc8c83510596fd21c3241bfc3e7a6a399
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-02 19:34:02 +0530

    commit

--- crtMay1.cpp
+++ crtMay1.cpp
@@ -1,14 +1,35 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void myPrint(int n){
-    for(int i =1;i<=n;i++){
-        cout<<n<<" ";
-    }
-    cout<<endl;
-}
-int main(){
-    int n = 122;
-    myPrint(n);
+// void myPrint(int n){
+//     for(int i =1;i<=n;i++){
+//         cout<<n<<" ";
+//     }
+//     cout<<endl;
+// }
+// int multiply(int a, int b)
+// {
+//     if (a == 0)
+//     {
+//         return 0;
+//     }
+//     else
+//     {
+//         return b + multiply(--a, b);
+//     }
+// }
+int main()
+{
+    // int n = 122;
+    // myPrint(n);
+    // int a, b;
+    // cout << "Enter fist number: ";
+    // cin >> a;
+    // cout << endl;
+    // cout << "Enter second number: ";
+    // cin >> b;
+    // cout << endl;
+    // int ans = multiply(a, b);
+    // cout << ans << endl;
 
     return 0;
 }

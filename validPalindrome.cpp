01297636a194895079c6d5c63a4bf8324ae7c20f
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-04 21:46:10 +0530

    commit

--- validPalindrome.cpp
+++ validPalindrome.cpp
@@ -1,56 +1,19 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h> 
 using namespace std;
-bool isPalindrome(string s)
+bool checkPalindrome(string s)
 {
-    string ans;
-    for (int i = 0; i < s.length(); i++)
-    {
-        if(s[i]==' '){
-            continue;
+    transform(s.begin(),s.end(),s.begin(), ::tolower);
+    string newstr;
+    for(int i=0;i<s.size();i++){
+        if(int(s[i]>=97 && int(s[i])<=122)){
+            newstr = newstr + s[i];
         }
-        else if(s[i]>='A' || s[i]<='Z'){
-            cout<<" transformation working"<<endl;
-            ans = ans + char(int(s[i])+32);
-        }
-        else  if(s[i]>='a' || s[i]<='z'){
-            ans = ans + s[i];
-        }
-        
-
     }
-    cout<<ans<<endl;
-    int i = 0;
-    int con = 0;
-    int l = ans.length() - 1;
-    if (l >=1)
-    {
-        while (i < l)
-        {
-            if (ans[i] == ans[l])
-            {
-                con = 1;
-            }
-            else
-            {
-                con = 0;
-                break;
-            }
-            i++;
-            l--;
-        }
-        return con;
-    }
-    else
-    {
-        return 1;
-    }
+    cout<<newstr<<endl;
 }
 
 int main(){
-    string s;
-    getline(cin,s);
-    cout<<isPalindrome(s);
+    string s="N2 i&nJA?a& jnI2n";
+    checkPalindrome(s);
     return 0;
 }

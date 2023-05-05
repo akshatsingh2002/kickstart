3338e17b28ea2b6020dd9e771510286eaacd918f
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-05 21:49:04 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,20 @@
-string l;
-    for(int i=0;i<s.length();i++){
-        if(s.at(i)==' '){
-            l = l + "@40";
-        }
-        else{
-            l = l + s[i];
+transform(s.begin(), s.end(), s.begin(), ::tolower);
+    string newstr;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 0 && s[i] <= 9))
+        {
+            newstr = newstr + s[i];
         }
     }
-    cout<<l<<endl;
+    int i = 0, l = newstr.length() - 1;
+    while (i < l)
+    {
+        if (newstr[i] != newstr[l])
+        {
+            return 0;
+        }
+        i++;
+        l--;
+    }
+    return 1;
--- validPalindrome.cpp
+++ validPalindrome.cpp
@@ -1,27 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool checkPalindrome(string s)
-{   cout<<"Input taken"<<endl;
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
-    string newstr;
-    for (int i = 0; i < s.length(); i++)
-    {
-        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 0 && s[i] <= 9))
-        {
-            newstr = newstr + s[i];
+{   
+    // transform(s.begin(), s.end(), s.begin(), ::tolower);
+    // string newstr;
+    // for (int i = 0; i < s.length(); i++)
+    // {
+    //     if ((s[i] >= 'a' && s[i] <= 'z') || (int(s[i]) >= 48 && int(s[i]) <= 57))
+    //     {
+    //         newstr = newstr + s[i];
+    //     }
+    // }
+    // int i = 0, l = newstr.length() - 1;
+    // cout<<newstr<<endl;
+    // while (i < l)
+    // {
+    //     if (newstr[i] != newstr[l])
+    //     {
+    //         return 0;
+    //     }
+    //     i++;
+    //     l--;
+    // }
+    // return 1;
+    for(int i=0;i<s.length();i++){
+        if(s[i]==' '){
+            s.erase(i,1);
+        }
+        else if(s[i]>='A' && s[i]<='Z'){
+            s[i] = s[i] - 'A'+'a';
         }
     }
-    int i = 0, l = newstr.length() - 1;
-    while (i < l)
-    {
-        if (newstr[i] != newstr[l])
-        {
-            return 0;
-        }
-        i++;
-        l--;
-    }
-    return 1;
+    cout<<s<<endl;
 }
 
 int main()

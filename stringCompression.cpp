7c21762b49d3b758d4ea247bab84c73683f0376f
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-08 19:47:57 +0530

    commit

--- stringCompression.cpp
+++ stringCompression.cpp
@@ -3,35 +3,41 @@
 using namespace std;
 int compress(vector<char> &chars)
 {
-  int final=0;
-  int count = 1;
-  for(int i=0;i<chars.size()-1;i++){
-        if(chars[i]==chars[i+1] || i!=chars.size()-1){
+    int final = 0;
+    int count = 1;
+    for (int i = 0; i < chars.size() - 1; i++)
+    {
+        if (chars[i] == chars[i + 1] && i != chars.size() - 2)
+        {
             count++;
         }
-        else{
-            if(count>=100){
+        else
+        {
+            if (count >= 100)
+            {
                 final = final + 4;
             }
-            else if(count>=10){
+            else if (count >= 10)
+            {
                 final = final + 3;
             }
-            else if(count>=2){
+            else if (count >= 2)
+            {
                 final = final + 2;
             }
-            else{
+            else
+            {
                 final = final + 1;
             }
             count = 1;
         }
-  }
-  cout<<final<<endl;
-
+    }
+    cout << final << endl;
 }
 
 int main()
 {
-    vector<char> arr = {'a','a','b','b','c','c','c'};
+    vector<char> arr = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
     compress(arr);
 
     return 0;

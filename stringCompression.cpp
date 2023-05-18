4be4f9d25ff780b0201f8e54a16142cef74e4244
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-18 21:57:11 +0530

    commit

--- stringCompression.cpp
+++ stringCompression.cpp
@@ -1,19 +1,46 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
- int compress(vector<char>& chars){
-   int l = chars.size();
-   
+int compress(vector<char> &chars)
+{
+     vector<char> temp;
+            int count = 1;
+            for(int i=0;i<chars.size()-1;i++){
+                if(chars[i]==chars[i+1]){
+                    count ++;
+                }
+                if(i==chars.size()-2){
+                    if(chars[i]==chars[i+1]){
+                        count ++;
+                    }
+                }
+                else{
+                    if(count == 1){
+                        temp.push_back(chars[i]);
+                    }
+                    else{
+                        temp.push_back(chars[i]);
+                        string val = to_string(count);
+                        temp.push_back(val);
+                        count = 1;
+                    }
+                }
+            }
+            chars.clear();
+            chars = temp;
+            return temp.size();
+}
 
- }
-
- int main(){
-   vector<char>  arr ={'a','a','b','b','c','c','c'};
+int main()
+{
+   vector<char> arr = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
    compress(arr);
-   for(int i=0;i<arr.size();i++){
-      cout<<arr[i]<<endl;
+   cout<<"final ouput"<<endl;
+   for (int i = 0; i < arr.size(); i++)
+   {
+      cout << arr[i] << endl;
    }
-    return 0;
- }
+   return 0;
+}

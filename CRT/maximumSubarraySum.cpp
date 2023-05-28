c654cd35aba9b1f2f67254c418f3295de51d6f36
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-05-28 07:49:13 +0530

    commit

--- CRT/maximumSubarraySum.cpp
+++ CRT/maximumSubarraySum.cpp
@@ -1,20 +1,24 @@
-#include<iostream>
-#include<vector>
-#include<limits.h>
+#include <iostream>
+#include <vector>
+#include <limits.h>
 using namespace std;
-int maxSubArray(vector<int>& nums) {
-        int presum=0,minsum=0,ans=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            presum+=nums[i];
-            ans = max(ans,presum-minsum);
-            minsum = min(minsum,presum);
-        }
-        return ans;
+int maxSubArray(vector<int> &nums)
+{
+    int presum = 0, minsum = 0, ans = INT_MIN;
+    // int l , r;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        presum += nums[i];
+        ans = max(ans, presum - minsum);
+        minsum = min(minsum, presum);
     }
+    return ans;
+}
 
-int main(){
-    vector<int> arr= {-2,1,-3,4,-1,2,1,-5,4};
+int main()
+{
+    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int val = maxSubArray(arr);
-    cout<<val<<endl;
+    cout << val << endl;
     return 0;
 }

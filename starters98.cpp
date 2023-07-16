041f19fd522373055c4b06629f21d11af75d11d4
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2023-07-16 19:29:53 +0530

    commit

---
+++ starters98.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+// void football(){
+//     long long   a,b,x,y;
+//     cin>>a>>b>>x>>y;
+//     long long ans1 =a*2 + b;
+//     long long ans2 =x*2+ y;
+//     if(ans1==ans2){
+//         cout<<"Equal"<<endl;
+//     }
+//     else if(ans1>ans2){
+//         cout<<"Missi"<<endl;
+//     }
+//     else{
+//         cout<<"Ronaldo"<<endl;
+//     }
+// }
+int main(){
+    return 0;
+}

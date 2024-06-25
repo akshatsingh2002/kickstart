864019f0eef61f0ac47bc3cc90e71a408afb7843
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2024-06-25 17:19:06 +0530

    commit

--- manhattan_circle.cpp
+++ manhattan_circle.cpp
@@ -1,21 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void solve(){
-    int h,k;
-    cin>>h>>k;
-    char arr[h][k];
-    int ans_a,ans_b;
-    int max=0;
-    for(int i=0;i<h;i++){
-        for(int j=0;j<k;j++){
-            cin>>arr[i][j];
+void solve()
+{
+    int h, k;
+    cin >> h >> k;
+    int ans_a, ans_b;
+    char temp;
+    int max = 0;
+    int flag1 = 0;
+    int r;
+    for (int i = 1; i <= h; i++)
+    {
+        int count = 0;
+        for (int j = 1; j <= k; j++)
+        {
+            cin >> temp;
+            if (temp == '#' && flag1 == 0)
+            {
+                ans_b = j;
+            }
+            if (temp == '#')
+            {
+                count++;
+            }
         }
+        // cout<<count<<endl;
+        if (count > max)
+        {
+            ans_a = i;
+            max = count;
+        }
+        count = 0;
     }
+    cout << ans_a << " " << ans_b << endl;
 }
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         solve();
     }
     return 0;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,1 @@
- string s1 = "australia";
-    string s2 = "yqxtuzmvbk";
-    int k = 1;
+ // cout<<count<<endl;
